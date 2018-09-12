2e85142b83c810c8bc8b0a473076ddaccb93d053
laggingreflex <laggingreflex@gmail.com>
2018-09-12 14:55:00 +0530

    feat: better error handling
    switch back to using native spawn (from foreground-child) in order to get control back with a status code
    
    Add warning in case of erroneous exits

--- index.js
+++ index.js
@@ -1,33 +1,57 @@
-const foreground = require('foreground-child');
+const { spawnSync } = require('child_process');
+
+const warn = () => console.warn('Note: This command was run via npm module \'win-node-env\'');
+const exit = (...error) => {
+  if (error) {
+    warn();
+    console.error('Error:', ...error);
+    process.exitCode = 1;
+  }
+}
 
 module.exports = firstEnvVar => {
+
+  if (!process.argv[2]) {
+    exit('No command was given');
+    return;
+  }
+
   process.argv[2] = firstEnvVar + process.argv[2];
   const args = process.argv.slice(2)
   // .reduce((args, arg) => args.concat(arg.split(/[ ]+/g)), [])
 
   const env = {};
   const cmd = [];
+
   let envDone;
   for (const arg of args) {
     if (envDone) {
       cmd.push(arg);
+    } else if (!arg.match(/=/)) {
+      envDone = true;
+      cmd.push(arg);
     } else {
-      if (!arg.match(/=/)) {
-        envDone = true;
-        cmd.push(arg);
-        continue;
-      }
       const [envVar, value] = arg.split('=');
       if (!envVar || !value) {
-        envDone = true;
-        cmd.push(arg);
-        continue;
+        exit(`Invalid env var:`, arg);
+        return;
+      } else {
+        env[envVar] = value;
       }
-      env[envVar] = value;
     }
+  }
+
+  if (!cmd.length) {
+    exit('No command was given');
+    return;
   }
 
   Object.assign(process.env, env);
 
-  foreground(cmd[0], cmd.slice(1));
+  const result = spawnSync(cmd[0], cmd.slice(1), { stdio: 'inherit', shell: true });
+  process.exitCode = result.status;
+
+  if (process.exitCode) {
+    warn();
+  }
 };
