3fe2b73bbea126574cc706351b849724b31f52de
laggingreflex <laggingreflex@gmail.com>
2018-09-12 10:20:27 +0530

    feat: use foreground-child (for better stdio/exit codes management)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const spawn = require('child_process').spawn
+const foreground = require('foreground-child')
 
 const env = process.argv[2].substr(1)
 const cmd = process.argv[3]
@@ -8,8 +8,4 @@ const args = process.argv.slice(4)
 
 process.env.NODE_ENV = env
 
-spawn(cmd, args, {
-  stdio: 'inherit',
-  env: process.env,
-  shell: true
-})
+foreground(cmd, args);
