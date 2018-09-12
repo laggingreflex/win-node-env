78bd11d320260b4c5dd03b38544442b548e6ea26
laggingreflex <laggingreflex@gmail.com>
2018-09-12 14:03:19 +0530

    fix: add tests

--- test.js
+++ test.js
@@ -1,7 +1,41 @@
-const assert = require('assert')
-const execSync = require('child_process').execSync
+const assert = require('assert');
+const exec = cmd => require('child_process').execSync(cmd, { encoding: 'utf8' });
 
-// This project should be `npm link`ed
-assert.equal('ok', execSync('NODE_ENV=ok node -e "process.stdout.write(process.env.NODE_ENV)"', { encoding: 'utf8' }))
+const multiple = ex => e => it(e, () => {
+  const [, bin, args] = e.match(/([\w_]+)(=.*)/);
+  const output = JSON.parse(exec(`${ex}${bin} ${args} node -e "process.stdout.write(JSON.stringify(process.env))"`));
+  const env = e.split(' ').reduce((env, e) => ({ ...env, [e.split('=')[0]]: e.split('=')[1] }), {});
+  for (const [key, val] of Object.entries(env))
+    assert.deepEqual([key, val], [key, output[key]]);
+});
 
-console.log('pass')
+describe('bin', () => {
+  const e = e => it(e, () => assert.equal('ok', exec(`node bin/${e} =ok node -e "process.stdout.write(process.env.${e})"`)));
+  e('NODE_ENV');
+  e('ENV');
+  e('DEBUG');
+  e('PORT');
+  describe('multiple', () => {
+    const e = multiple('node bin/');
+    e('NODE_ENV=1 ENV=2');
+    e('ENV=1 NODE_ENV=2');
+    e('PORT=1 ENV=2');
+    e('DEBUG=1 ENV=2');
+  });
+});
+
+describe('CLI', () => {
+  /* This module must be npm-link'ed (abd globally accessible) for this test to work */
+  const e = e => it(e, () => assert.equal('ok', exec(`${e}=ok node -e "process.stdout.write(process.env.${e})"`)));
+  e('NODE_ENV');
+  e('ENV');
+  e('DEBUG');
+  e('PORT');
+  describe('multiple', () => {
+    const e = multiple('');
+    e('NODE_ENV=1 ENV=2');
+    e('ENV=1 NODE_ENV=2');
+    e('PORT=1 ENV=2');
+    e('DEBUG=1 ENV=2');
+  });
+});
