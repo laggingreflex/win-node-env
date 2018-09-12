23ccd3f7bf70fdfc250e9fec3239e1e8d14bd24c
laggingreflex <laggingreflex@gmail.com>
2018-09-12 12:31:10 +0530

    refactor, separate NODE_ENV.js
    pave way for more first env vars

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-#!/usr/bin/env node
-
 const foreground = require('foreground-child');
 
-process.argv[2] = 'NODE_ENV' + process.argv[2];
-const args = process.argv.slice(2)
-// .reduce((args, arg) => args.concat(arg.split(/[ ]+/g)), [])
+module.exports = firstEnvVar => {
+  process.argv[2] = firstEnvVar + process.argv[2];
+  const args = process.argv.slice(2)
+  // .reduce((args, arg) => args.concat(arg.split(/[ ]+/g)), [])
 
-const env = {};
-const cmd = [];
-let envDone;
-for (const arg of args) {
-  if (envDone) {
-    cmd.push(arg);
-  } else {
-    if (!arg.match(/=/)) {
-      envDone = true;
+  const env = {};
+  const cmd = [];
+  let envDone;
+  for (const arg of args) {
+    if (envDone) {
       cmd.push(arg);
-      continue;
+    } else {
+      if (!arg.match(/=/)) {
+        envDone = true;
+        cmd.push(arg);
+        continue;
+      }
+      const [envVar, value] = arg.split('=');
+      if (!envVar || !value) {
+        envDone = true;
+        cmd.push(arg);
+        continue;
+      }
+      env[envVar] = value;
     }
-    const [envVar, value] = arg.split('=');
-    if (!envVar || !value) {
-      envDone = true;
-      cmd.push(arg);
-      continue;
-    }
-    env[envVar] = value;
   }
-}
 
-Object.assign(process.env, env);
+  Object.assign(process.env, env);
 
-foreground(cmd[0], cmd.slice(1));
+  foreground(cmd[0], cmd.slice(1));
+};
